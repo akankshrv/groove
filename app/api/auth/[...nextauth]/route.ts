c25bf967575deefa50bc116e271781a183ea6305
AKANKSH RAMANAGARA VIJAYKUMAR <akankshramanagaravijaykumar@AKANKSHs-Laptop.local>
2024-09-11 11:04:49 -0700

    Added routes

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,13 +1,32 @@
+import { prismaClient } from "@/app/lib/db";
 import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
 
 const handler = NextAuth({
-    providers: [
+  providers: [
     GoogleProvider({
-    clientId: process.env.GOOGLE_CLIENT_ID ?? "",
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? ""
-  })
-    ]
-})
+      clientId: process.env.GOOGLE_CLIENT_ID ?? "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
+    }),
+  ],
+  callbacks: {
+    async signIn(params) {
+      if (!params.user.email) {
+        return false;
+      }
+      console.log(params);
+      try {
+        await prismaClient.user.create({
+          data: {
+            email: params.user.email,
+            provider: "Google",
+          },
+        });
+      } catch (e) {}
+
+      return true;
+    },
+  },
+});
 
 export { handler as GET, handler as POST };
